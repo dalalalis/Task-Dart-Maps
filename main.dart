7aada6c644b610e5ed2310feb719618e0b09d4c1
Dalal Alsabah <dasabah@gmail.com>
2022-11-11 11:51:32 +0300

    task solved including the challenge

--- main.dart
+++ main.dart
@@ -1,6 +1,34 @@
-void main(){
-  
-  
-  
-  
+void main() {
+  Map<String, dynamic> menu = {
+    'burger': 6.5,
+    'pizza': 5,
+    'water': 1.5,
+  };
+  menu["chocolate cake"] = 3;
+  menu["water"] = 0.750;
+  print('Menu:');
+  print("");
+  for (var item in menu.entries) {
+    print('${item.key} -> ${item.value}KD');
+  }
+  order(menu, ['water', 'burger', 'rice']);
 }
+
+void order(Map<String, dynamic> menu, List<String> order) {
+  double total = 0;
+  order.forEach((item) {
+    if (menu[item] != null) {
+      total += menu[item];
+    } else {
+      print('this $item does not exist');
+    }
+  });
+  print(total);
+}
+
+    
+  /*order.forEach((item) => menu[item] != null
+      ? total += menu[item]
+      : print('the ${menu[item]} is not on the menu'));
+  return total;
+}*/
